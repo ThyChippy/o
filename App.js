857cc81ed23ae253e8823d11d165efeee2bbf429
Elizabeth <123522433+ThyChippy@users.noreply.github.com>
2023-11-09 16:19:42 +0000

    the basic layout for todolist

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Platform, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './components/tasks';
+import Collapsible from 'react-native-collapsible';
+import Todo from './components/todo';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        <Todo title={"University Tasks"}/>
+        <Todo title={"Travel Tasks"}/>
+        <Todo title={"Shopping Tasks"}/>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +21,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  scrollView: {
+    marginHorizontal: 5,
+  },
+  tasksWrapper :{
+      paddingTop: 50,
+      paddingHorizontal:20,
+    },
+    sectionTitle :{
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+    item :{
+      marginTop: 30,
+    },
+    writeTaskWrapper: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    input: {
+      paddingVertical: 15,
+      paddingHorizontal: 15,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      borderColor: '#C0C0C0',
+      borderWidth: 1,
+      width: 250,
+    },
+    addWrapper: {
+      width: 60,
+      height: 60,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      justifyItems: 'center',
+      alignItems: 'center',
+      borderColor: '#C0C0C0',
+      borderWidth: 1,
+    },
+    addText: {
+    },
 });
---
+++ components/todo.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import { Platform, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, TouchableOpacity, Keyboard } from 'react-native';
+import Task from './tasks';
+import Collapsible from 'react-native-collapsible';
+
+const Todo = ({title}) => {
+    const [task, setTask] = useState();
+    const [taskItems, setTaskItems] = useState([]);
+
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask(null);
+    }
+
+    const completeTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy);
+    }
+
+    const [collasped, setCollasped] = useState(false);
+    const toggleExpand = () => {
+        setCollasped(!collasped)
+    }
+
+    return(
+        <View style={styles.tasksWrapper}>
+            <TouchableOpacity onPress={toggleExpand}>
+                <Text style={styles.sectionTitle}>{title}</Text>
+            </TouchableOpacity>
+            <Collapsible collapsed={collasped}>
+                <View style={styles.item}>
+                 {/* where the tasks go */}
+                {
+                taskItems.map((item, index)=>{
+                    return (
+                    <TouchableOpacity key={index} onPress={()=>completeTask(index)}>
+                    <Task key={index} text={item}/>
+                    </TouchableOpacity>
+                    )
+                })
+                }
+                {/* <Task/> */}
+                </View>
+                <KeyboardAvoidingView
+                behaviour={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.writeTaskWrapper}>
+                    <TextInput style={styles.input} placeholder={'add task'} value={task} onChangeText={text=>setTask(text)}/>
+                    <TouchableOpacity onPress={() => handleAddTask()}>
+                        <View style={styles.addWrapper}>
+                            <Text style={styles.addText}>+</Text>
+                        </View>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
+            </Collapsible>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    tasksWrapper :{
+        paddingTop: 50,
+        paddingHorizontal:20,
+      },
+      sectionTitle :{
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      item :{
+        marginTop: 20,
+      },
+      writeTaskWrapper: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+      },
+      input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        width: 250,
+      },
+      addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        justifyItems: 'center',
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+      },
+      addText: {
+      },
+});
+
+
+
+export default Todo;
